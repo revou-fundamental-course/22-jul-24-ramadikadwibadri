69264e08d1cad73d18df80f2ec10f19ff3ff3773
ramadika dwi <ramadikadwibadri@gmail.com>
2024-08-01 15:08:58 +0700

    revisi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // ini javascript
+
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const address = document.forms["message-form"]["address"].value;
+    const gender =document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || address =="" || gender =="" || messages =="") {
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    }
+    setSenderUI(name, address, gender, messages);
+    return false;
+
+}
+ function setSenderUI(name, address, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-address").innerHTML = address;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+ }
